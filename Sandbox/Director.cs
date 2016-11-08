13086eb991c6c6a717a5b1ceebf67ef9d94dc2f8
Christian <Christian@WORKSTATION>
2016-11-08 13:52:18 +0100

    More

---
+++ Sandbox/Director.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Director : Manager
+    {
+       
+        public Director(string name, int salaryPerMonth, int monthlyBonus, int workhrs)
+            : base(name, salaryPerMonth, monthlyBonus, workhrs)
+        {
+        }
+
+        public override int GetSalaryPerMonth()
+        {
+            int salary = base.GetSalaryPerMonth() + 20000;
+            return salary;
+        }
+    }
+}
