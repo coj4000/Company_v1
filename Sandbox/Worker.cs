3c004ff1bdaa764086a9e22e283ca414c7e9e964
Christian <Christian@WORKSTATION>
2016-11-08 13:12:03 +0100

    Opg 1

---
+++ Sandbox/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker : Employee
+    {
+        private string skill;
+        public Worker(string name, int salaryPerMonth, string skill) : base(name, salaryPerMonth)
+        {
+            
+        }
+    }
+}
